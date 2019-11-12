d21c04ccfed54339a2f1f5929320e31b03672ae4
Yigong Hu <hyigong1@jhu.edu>
2019-11-12 14:24:30 -0500

    Add table with MyISAM

--- Linux/s2e_home/launch.sh
+++ Linux/s2e_home/launch.sh
@@ -82,6 +82,31 @@ sleep 60
 ./bin/mysql -S mysqld.sock << EOF
 use test;
 CREATE TABLE tbl(id INT NOT NULL AUTO_INCREMENT,col INT NOT NULL, PRIMARY KEY (id)) Engine = InnoDB;
+INSERT INTO tbl(col) VALUES(11);
+INSERT INTO tbl(col) VALUES(12);
+INSERT INTO tbl(col) VALUES(13);
+INSERT INTO tbl(col) VALUES(14);
+INSERT INTO tbl(col) VALUES(15);
+INSERT INTO tbl(col) VALUES(16);
+INSERT INTO tbl(col) VALUES(17);
+INSERT INTO tbl(col) VALUES(18);
+INSERT INTO tbl(col) VALUES(19);
+INSERT INTO tbl(col) VALUES(20);
+INSERT INTO tbl(col) VALUES(21);
+INSERT INTO tbl(col) VALUES(22);
+INSERT INTO tbl(col) VALUES(23);
+INSERT INTO tbl(col) VALUES(24);
+INSERT INTO tbl(col) VALUES(25);
+INSERT INTO tbl(col) VALUES(26);
+INSERT INTO tbl(col) VALUES(27);
+INSERT INTO tbl(col) VALUES(28);
+INSERT INTO tbl(col) VALUES(29);
+INSERT INTO tbl(col) VALUES(30);
+CREATE TABLE tbl1(id INT NOT NULL AUTO_INCREMENT,col INT NOT NULL, PRIMARY KEY (id)) Engine = MyISAM;
+INSERT INTO tbl1(col) VALUES(31);
+INSERT INTO tbl1(col) VALUES(32);
+INSERT INTO tbl1(col) VALUES(33);
+INSERT INTO tbl1(col) VALUES(34);
 EOF
 ./bin/mysqladmin -S mysqld.sock -u root shutdown
 cd ../../..
